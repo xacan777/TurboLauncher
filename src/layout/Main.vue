efd3c8ca67400e16dd942d0b475507cf42839684
xacan777 <120599423+xacan777@users.noreply.github.com>
2025-12-26 01:30:02 +0300

    Add .eslintrc.json so eslint config is detected

--- src/layout/Main.vue
+++ src/layout/Main.vue
@@ -4,47 +4,20 @@ Header
   router-view(v-slot="{ Component }")
     transition(name="fade" mode="out-in")
       component(:is="Component")
-DirSettings
 </template>
 
 <script>
-import DirSettings from '@/components/main/DirSettings.vue';
 import Header from '@/components/main/Header.vue';
 import utilsMixin from '@/mixins/utils-mixin';
-import { mapGetters, mapActions } from 'vuex';
 
 export default {
   name: 'MainLayout',
-  components: { Header, DirSettings },
+  components: { Header },
   mixins: [utilsMixin],
-  data: () => ({
-    intervalBalance: null,
-  }),
   computed: {
-    ...mapGetters(['userBase', 'user', 'isAfterAuthorized']),
     isWeb() {
       return typeof this.$route.meta?.isWeb === 'undefined' ? false : this.$route.meta.isWeb;
     },
-  },
-  mounted() {
-    this.$store.dispatch('fetchBalanceUser');
-    this.intervalBalance = setInterval(() => {
-      this.$store.dispatch('fetchBalanceUser');
-    }, 5000);
-
-    if (this.isAfterAuthorized) {
-      this.setIsClientReady(true);
-    } else if (this.userBase.login && !this.isAfterAuthorized) {
-      setTimeout(() => {
-        this.onReauthForGenerationToken();
-      }, 3000);
-    }
-  },
-  methods: {
-    ...mapActions(['logoutUser', 'setIsClientReady', 'setClientParams']),
-  },
-  unmounted() {
-    clearInterval(this.intervalBalance);
   },
 };
 </script>
@@ -52,10 +25,11 @@ export default {
 <style scoped>
 .content {
   height: 100%;
+  padding-top: 16px;
 }
 
 .web {
-  padding-top: 230px;
+  padding-top: 24px;
   box-sizing: border-box;
   max-width: 920px;
   width: 100%;
